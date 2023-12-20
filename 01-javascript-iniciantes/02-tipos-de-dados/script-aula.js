5360810e8bfc5d2d3212448e6e105024224d9172
gabriel <gabriellima0409@gmail.com>
2023-12-20 08:14:01 -0300

    tipos de dados

---
+++ 01-javascript-iniciantes/02-tipos-de-dados/script-aula.js
@@ -0,0 +1,22 @@
+var nome = 'Gabriel';
+var idade = 24;
+var time = null;
+console.log(typeof time);
+
+// String
+
+var nome = 'Gabriel';
+var sobrenome = 'Lima';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romário fez' + gols + 'gols';
+console.log(frase);
+
+var frase1 = 'Esse é o "melhor jogo"';
+
+var gols = 1000;
+var frase = `Romário fez ${gols * 2} gols`;
+console.log(frase);
---
+++ 01-javascript-iniciantes/02-tipos-de-dados/script.js
@@ -0,0 +1,11 @@
+var palavra = 'oi';
+var num = '20';
+var idade = 24;
+var nome = 'Gabriel';
+var sobrenome = 'Lima';
+
+var nomeCompleto = nome + sobrenome;
+
+var frase = `'It's time`;
+
+console.log(typeof nome);
